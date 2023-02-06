fe47d33f5a1a1fdb4b3ca7346c956ac850b3330a
Phung Tan Phuong <phuong.phung@beetech.com.vn>
2023-02-07 01:20:55 +0700

    fix xem don hang

--- php/apixemdonhang.php
+++ php/apixemdonhang.php
@@ -30,120 +30,191 @@ switch ($event) {
 
     case "getALLDonHangTrongNgay":
         $mang = array();
-        $search = $_POST['search']; //Tìm kiếm dữ liệu
+        // $search = $_POST['search']; //Tìm kiếm dữ liệu
         //định dạng giờ
-        $f1 = "00:00:00";
-        $from = date('Y-m-d') . " " . $f1;
-        $t1 = "23:59:59";
-        $to = date('Y-m-d') . " " . $t1;
-        $sql = mysqli_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v where v.trangthaidh = 0 and v.ngaydh between '" . $from . "' and '" . $to . "' and (v.hotenkh like '%" . $search . "%' or v.sodh like '%" . $search . "%') order by v.sodh asc ");
-
-        while ($rows = mysqli_fetch_array($sql)) {
-            $mangctsp = array();
-            $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
-            while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
-                $usersp['masp'] = $rowctsp['masp'];
-                $usersp['tensp'] = $rowctsp['tensp'];
-                array_push($mangctsp, $usersp);
-            }
-            $usertemp['sodh'] = $rows['sodh'];
-            $usertemp['makh'] = $rows['makh'];
-            $usertemp['hotenkh'] = $rows['hotenkh'];
-            $usertemp['email'] = $rows['email'];
-            $usertemp['sdt'] = $rows['sdt'];
-            $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
-            $usertemp['ngaydh'] = $rows['ngaydh'];
-            $usertemp['tongtien'] = $rows['tongtien'];
-            $usertemp['trangthaidh'] = $rows['trangthaidh'];
-            $usertemp['chitietdh'] = $mangctsp;
-            array_push($mang, $usertemp);
-            $mangctsp = null;
+        // $f1 = "00:00:00";
+        // $from = date('Y-m-d') . " " . $f1;
+        // $t1 = "23:59:59";
+        // $to = date('Y-m-d') . " " . $t1;
+        $sql = sqlsrv_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v", array(), array( "Scrollable" => 'static' ));
+        if (!$sql) {
+            die(print_r(sqlsrv_errors(), true));
         }
-        $jsonData['items'] = $mang;
-        echo json_encode($jsonData);
-        mysqli_close($conn);
+        header("Content-Type: application/json");
+
+        if (sqlsrv_num_rows($sql) > 0) {
+
+            while ($rows = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+                // $mangctsp = array();
+                // $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
+                // while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
+                //     $usersp['masp'] = $rowctsp['masp'];
+                //     $usersp['tensp'] = $rowctsp['tensp'];
+                //     array_push($mangctsp, $usersp);
+                // }
+
+                $usertemp['sodh'] = $rows['sodh'];
+                $usertemp['makh'] = $rows['makh'];
+                $usertemp['hotenkh'] = $rows['hotenkh'];
+                $usertemp['email'] = $rows['email'];
+                $usertemp['sdt'] = $rows['sdt'];
+                $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
+                $usertemp['ngaydh'] = $rows['ngaydh'];
+                $usertemp['tongtien'] = $rows['tongtien'];
+                $usertemp['trangthaidh'] = $rows['trangthaidh'];
+                $usertemp['chitietdh'] = $mangctsp;
+                array_push($mang, $usertemp);
+            }
+            $jsondata['success'] = 1;
+            $jsondata['items'] = $mang;
+        
+            echo json_encode($jsondata);
+        } else {
+            $jsondata['success'] = 0;
+            $jsondata['items'] = $mang;
+            echo json_encode($jsondata);
+        }
+
+
+        
+        // $jsonData['items'] = $mang;
+        // echo json_encode($jsonData);
+        sqlsrv_close($conn);
         break;
 
     case "getALLSPDonHang":
         $mang = array();
-        $sodh = $_POST['sodh'];
-        $sql = mysqli_query($conn, "select c.masp, s.tensp, c.soluong, c.giatien, c.giamgia, thanhtien(c.soluong,c.giatien,c.giamgia) as thanhtien, v.tongtien from chitietdh c, sanpham s, view_donhang v where c.sodh = v.sodh and c.masp = s.masp and c.sodh = '" . $sodh . "'");
-        while ($rows = mysqli_fetch_array($sql)) {
-            $usertemp['masp'] = $rows['masp'];
-            $usertemp['tensp'] = $rows['tensp'];
-            $usertemp['soluong'] = $rows['soluong'];
-            $usertemp['giatien'] = $rows['giatien'];
-            $usertemp['giamgia'] = $rows['giamgia'];
-            $usertemp['thanhtien'] = $rows['thanhtien'];
-            $usertemp['tongtien'] = $rows['tongtien'];
-            array_push($mang, $usertemp);
+        // $sodh = $_POST['sodh'];
+        $sql = sqlsrv_query($conn, "select c.masp, s.tensp, c.soluong, c.giatien, c.giamgia, thanhtien(c.soluong,c.giatien,c.giamgia) as thanhtien, v.tongtien from chitietdh c, sanpham s, view_donhang v where c.sodh = v.sodh and c.masp = s.masp", array(), array( "Scrollable" => 'static' ));
+        if (!$sql) {
+            die(print_r(sqlsrv_errors(), true));
         }
-        $jsonData['items'] = $mang;
-        echo json_encode($jsonData);
-        mysqli_close($conn);
+        header("Content-Type: application/json");
+
+        if (sqlsrv_num_rows($sql) > 0) {
+
+            while ($rows = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+                $usertemp['masp'] = $rows['masp'];
+                $usertemp['tensp'] = $rows['tensp'];
+                $usertemp['soluong'] = $rows['soluong'];
+                $usertemp['giatien'] = $rows['giatien'];
+                $usertemp['giamgia'] = $rows['giamgia'];
+                $usertemp['thanhtien'] = $rows['thanhtien'];
+                $usertemp['tongtien'] = $rows['tongtien'];
+                array_push($mang, $usertemp);
+            }
+            $jsondata['success'] = 1;
+            $jsondata['items'] = $mang;
+        
+            echo json_encode($jsondata);
+        } else {
+            $jsondata['success'] = 0;
+            $jsondata['items'] = $mang;
+            echo json_encode($jsondata);
+        }
+
+
+        // $jsonData['items'] = $mang;
+        // echo json_encode($jsonData);
+        sqlsrv_close($conn);
         break;
 
     case "getALLDonHangDangXuLy":
         $mang = array();
-        $search = $_POST['search']; //Tìm kiếm dữ liệu
-        $sql = mysqli_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v where (v.trangthaidh = 0 or v.trangthaidh = 1) and (v.hotenkh like '%" . $search . "%' or v.sodh like '%" . $search . "%') order by v.sodh asc ");
-
-        while ($rows = mysqli_fetch_array($sql)) {
-            $mangctsp = array();
-            $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
-            while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
-                $usersp['masp'] = $rowctsp['masp'];
-                $usersp['tensp'] = $rowctsp['tensp'];
-                array_push($mangctsp, $usersp);
-            }
-            $usertemp['sodh'] = $rows['sodh'];
-            $usertemp['makh'] = $rows['makh'];
-            $usertemp['hotenkh'] = $rows['hotenkh'];
-            $usertemp['email'] = $rows['email'];
-            $usertemp['sdt'] = $rows['sdt'];
-            $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
-            $usertemp['ngaydh'] = $rows['ngaydh'];
-            $usertemp['tongtien'] = $rows['tongtien'];
-            $usertemp['trangthaidh'] = $rows['trangthaidh'];
-            $usertemp['chitietdh'] = $mangctsp;
-            array_push($mang, $usertemp);
-            $mangctsp = null;
+        // $search = $_POST['search']; //Tìm kiếm dữ liệu
+        $sql = sqlsrv_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v where (v.trangthaidh = 0 or v.trangthaidh = 1)", array(), array( "Scrollable" => 'static' ));
+        if (!$sql) {
+            die(print_r(sqlsrv_errors(), true));
         }
-        $jsonData['items'] = $mang;
-        echo json_encode($jsonData);
-        mysqli_close($conn);
+        header("Content-Type: application/json");
+
+        if (sqlsrv_num_rows($sql) > 0) {
+
+            while ($rows = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+                // $mangctsp = array();
+                // $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
+                // while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
+                //     $usersp['masp'] = $rowctsp['masp'];
+                //     $usersp['tensp'] = $rowctsp['tensp'];
+                //     array_push($mangctsp, $usersp);
+                // }
+                $usertemp['sodh'] = $rows['sodh'];
+                $usertemp['makh'] = $rows['makh'];
+                $usertemp['hotenkh'] = $rows['hotenkh'];
+                $usertemp['email'] = $rows['email'];
+                $usertemp['sdt'] = $rows['sdt'];
+                $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
+                $usertemp['ngaydh'] = $rows['ngaydh'];
+                $usertemp['tongtien'] = $rows['tongtien'];
+                $usertemp['trangthaidh'] = $rows['trangthaidh'];
+                $usertemp['chitietdh'] = $mangctsp;
+                array_push($mang, $usertemp);
+                // $mangctsp = null;
+            }
+            $jsondata['success'] = 1;
+            $jsondata['items'] = $mang;
+        
+            echo json_encode($jsondata);
+        } else {
+            $jsondata['success'] = 0;
+            $jsondata['items'] = $mang;
+            echo json_encode($jsondata);
+        }
+
+        
+        // $jsonData['items'] = $mang;
+        // echo json_encode($jsonData);
+        sqlsrv_close($conn);
         break;
 
     case "getALLDonHangHoanThanh":
         $mang = array();
-        $search = $_POST['search']; //Tìm kiếm dữ liệu
-        $sql = mysqli_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v where (v.trangthaidh = 2 or v.trangthaidh = 3) and (v.hotenkh like '%" . $search . "%' or v.sodh like '%" . $search . "%') order by v.sodh asc ");
-
-        while ($rows = mysqli_fetch_array($sql)) {
-            $mangctsp = array();
-            $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
-            while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
-                $usersp['masp'] = $rowctsp['masp'];
-                $usersp['tensp'] = $rowctsp['tensp'];
-                array_push($mangctsp, $usersp);
-            }
-            $usertemp['sodh'] = $rows['sodh'];
-            $usertemp['makh'] = $rows['makh'];
-            $usertemp['hotenkh'] = $rows['hotenkh'];
-            $usertemp['email'] = $rows['email'];
-            $usertemp['sdt'] = $rows['sdt'];
-            $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
-            $usertemp['ngaydh'] = $rows['ngaydh'];
-            $usertemp['tongtien'] = $rows['tongtien'];
-            $usertemp['trangthaidh'] = $rows['trangthaidh'];
-            $usertemp['chitietdh'] = $mangctsp;
-            array_push($mang, $usertemp);
-            $mangctsp = null;
+        // $search = $_POST['search']; //Tìm kiếm dữ liệu
+        $sql = sqlsrv_query($conn, "select v.sodh, v.makh, v.hotenkh, v.email, v.sdt, v.diachigiaohang, v.ngaydh, v.tongtien, v.trangthaidh from view_donhang v where (v.trangthaidh = 2 or v.trangthaidh = 3)", array(), array( "Scrollable" => 'static' ));
+        if (!$sql) {
+            die(print_r(sqlsrv_errors(), true));
         }
-        $jsonData['items'] = $mang;
-        echo json_encode($jsonData);
-        mysqli_close($conn);
+        header("Content-Type: application/json");
+
+        if (sqlsrv_num_rows($sql) > 0) {
+
+            while ($rows = sqlsrv_fetch_array($sql, SQLSRV_FETCH_ASSOC)) {
+                // $mangctsp = array();
+                // $sqlctsp = mysqli_query($conn, "select c.masp, s.tensp from chitietdh c, sanpham s where c.masp = s.masp and c.sodh = '" . $rows['sodh'] . "'");
+                // while ($rowctsp = mysqli_fetch_array($sqlctsp)) {
+                //     $usersp['masp'] = $rowctsp['masp'];
+                //     $usersp['tensp'] = $rowctsp['tensp'];
+                //     array_push($mangctsp, $usersp);
+                // }
+                $usertemp['sodh'] = $rows['sodh'];
+                $usertemp['makh'] = $rows['makh'];
+                $usertemp['hotenkh'] = $rows['hotenkh'];
+                $usertemp['email'] = $rows['email'];
+                $usertemp['sdt'] = $rows['sdt'];
+                $usertemp['diachigiaohang'] = $rows['diachigiaohang'];
+                $usertemp['ngaydh'] = $rows['ngaydh'];
+                $usertemp['tongtien'] = $rows['tongtien'];
+                $usertemp['trangthaidh'] = $rows['trangthaidh'];
+                $usertemp['chitietdh'] = $mangctsp;
+                array_push($mang, $usertemp);
+                // $mangctsp = null;
+            }
+            $jsondata['success'] = 1;
+            $jsondata['items'] = $mang;
+        
+            echo json_encode($jsondata);
+        } else {
+            $jsondata['success'] = 0;
+            $jsondata['items'] = $mang;
+            echo json_encode($jsondata);
+        }
+
+       
+        // $jsonData['items'] = $mang;
+        // echo json_encode($jsonData);
+        sqlsrv_close($conn);
         break;
+        
     case "getBaoCaoDoanhThu":
         $mang = array();
         $datestart = $_POST['datestart'];
